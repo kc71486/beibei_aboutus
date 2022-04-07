988f2e630f154ece57c457935c168e5ca233b559
kc71486 <100687720+kc71486@users.noreply.github.com>
2022-04-07 20:15:56 +0800

    Add files via upload

---
+++ about_run.js
@@ -0,0 +1,8 @@
+{
+	document.getElementById("arrow_previous").addEventListener("mousedown", arrow_mousedown);
+	document.getElementById("arrow_previous").addEventListener("mouseup", arrow_mouseup);
+	document.getElementById("arrow_previous").addEventListener("click", arrow_click);
+	document.getElementById("arrow_next").addEventListener("mousedown", arrow_mousedown);
+	document.getElementById("arrow_next").addEventListener("mouseup", arrow_mouseup);
+	document.getElementById("arrow_next").addEventListener("click", arrow_click);
+}
