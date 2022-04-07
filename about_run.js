45d84f850622c7fc79088374d9ff91281cf6c6b1
kc71486 <100687720+kc71486@users.noreply.github.com>
2022-04-08 02:48:02 +0800

    Delete about_run.js

--- about_run.js
+++
@@ -1,8 +0,0 @@
-{
-	document.getElementById("arrow_previous").addEventListener("mousedown", arrow_mousedown);
-	document.getElementById("arrow_previous").addEventListener("mouseup", arrow_mouseup);
-	document.getElementById("arrow_previous").addEventListener("click", arrow_click);
-	document.getElementById("arrow_next").addEventListener("mousedown", arrow_mousedown);
-	document.getElementById("arrow_next").addEventListener("mouseup", arrow_mouseup);
-	document.getElementById("arrow_next").addEventListener("click", arrow_click);
-}
