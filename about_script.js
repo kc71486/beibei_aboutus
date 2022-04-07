ecaa5579fda01ef27d0c82606e553cbd0b1cc71e
kc71486 <100687720+kc71486@users.noreply.github.com>
2022-04-08 03:06:01 +0800

    Update about_script.js

--- about_script.js
+++ about_script.js
@@ -58,8 +58,15 @@ function updateMember() {
 
 
 function loadEvent() {
+	document.getElementById("arrow_previous").addEventListener("mousedown", arrow_mousedown);
+	document.getElementById("arrow_previous").addEventListener("mouseup", arrow_mouseup);
+	document.getElementById("arrow_previous").addEventListener("click", arrow_click);
+	document.getElementById("arrow_next").addEventListener("mousedown", arrow_mousedown);
+	document.getElementById("arrow_next").addEventListener("mouseup", arrow_mouseup);
+	document.getElementById("arrow_next").addEventListener("click", arrow_click);
 	changeTeampic();
 	changeTeamDescription();
+	document.getElementById("noload").style.opacity = 0;
 }
 
 function changeTeampic() {
